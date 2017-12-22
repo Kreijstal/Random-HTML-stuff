c3f5ae764557d596aec883d4aabb105e330d40f4
Kreijstal <2415206+Kreijstal@users.noreply.github.com>
2017-12-22 05:05:53 +0100

    Add files via upload

--- experiments/formParser.js
+++ experiments/formParser.js
@@ -150,8 +150,7 @@ formParse = /*
             }
           })
           var elementTypes = {
-            input: ["radio", "text", "color", "checkbox", "submit",
-              "number"],
+            input: ["button", "checkbox", "color", "date", "datetime", "datetime-local", "email", "file", "hidden", "image", "month", "number", "password", "radio", "range", "reset", "search", "submit", "tel", "text", "time", "url", "week"],
             noName: ["option"]
           }
 
